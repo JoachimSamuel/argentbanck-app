9f923e7c3f8f66ba2d6d86af8fedcf5f6bddf56c
Joachim Samuel <joachimsamuel@gmail.com>
2023-09-11 00:23:17 +0200

    Pages User Css OK

--- src/pages/User/Header.js
+++ src/pages/User/Header.js
@@ -13,15 +13,17 @@ const Header = () => {
       {isEditing ? (
         <form>
           <h1>Edit User Info</h1>
+          <div className='form-input'>
+            
           <label htmlFor="userName">User Name:</label>
           <input type="text" id="userName" placeholder="User Name" />
 
           <label htmlFor="firstName">First Name:</label>
-          <input type="text" id="firstName" placeholder="First Name" />
+          <input type="text" id="firstName" placeholder="First Name" readOnly/>
 
           <label htmlFor="lastName">Last Name:</label>
-          <input type="text" id="lastName" placeholder="Last Name" />
-
+          <input type="text" id="lastName" placeholder="Last Name" readOnly/>
+          </div>
           <div className="button-container">
             <Button  onClick={toggleEdit}>Save</Button>
             <Button  onClick={toggleEdit}>Cancel</Button>
